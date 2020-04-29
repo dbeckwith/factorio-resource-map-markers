923af31ba8b4a5a82f19f1c01b414aa594c43a50
Daniel Beckwith <djbsnx@gmail.com>
2020-04-28 21:03:58 -0400

    Search neighboring chunks as well

--- control.lua
+++ control.lua
@@ -1,3 +1,12 @@
+local function position_list_contains(l, p)
+  for _, p2 in pairs(l) do
+    if p.x == p2.x and p.y == p2.y then
+      return true
+    end
+  end
+  return false
+end
+
 local function list_concat(l1, l2)
   if #l1 == 0 then
     return l2
@@ -6,6 +15,53 @@ local function list_concat(l1, l2)
       table.insert(l1, bb)
     end
     return l1
+  end
+end
+
+local function remove_if(t, f)
+  local keys = {}
+  for key, el in pairs(t) do
+    if f(el) then
+      table.insert(keys, key)
+    end
+  end
+  for _, key in pairs(keys) do
+    table.remove(t, key)
+  end
+end
+
+local function merge_bbs(bb1, bb2)
+  if bb1 == nil then
+    return bb2
+  elseif bb2 == nil then
+    return bb1
+  else
+    return {
+      left_top = {
+        x = math.min(bb1.left_top.x, bb2.left_top.x),
+        y = math.min(bb1.left_top.y, bb2.left_top.y),
+      },
+      right_bottom = {
+        x = math.max(bb1.left_top.x, bb2.left_top.x),
+        y = math.max(bb1.left_top.y, bb2.left_top.y),
+      },
+    }
+  end
+end
+
+local function cardinal_neighbors(p)
+  local i = 0
+  return function()
+    i = i + 1
+    if i == 1 then
+      return { x = p.x - 1, y = p.y }
+    elseif i == 2 then
+      return { x = p.x + 1, y = p.y }
+    elseif i == 3 then
+      return { x = p.x, y = p.y - 1 }
+    elseif i == 4 then
+      return { x = p.x, y = p.y + 1 }
+    end
   end
 end
 
@@ -38,12 +94,12 @@ end
 
 local function bb_adjacent(bb1, bb2)
   local p1_min_x = math.floor(bb1.left_top.x)
-  local p1_max_x = math.ceil(bb1.right_bottom.x)
   local p1_min_y = math.floor(bb1.left_top.y)
+  local p1_max_x = math.ceil(bb1.right_bottom.x)
   local p1_max_y = math.ceil(bb1.right_bottom.y)
   local p2_min_x = math.floor(bb2.left_top.x)
-  local p2_max_x = math.ceil(bb2.right_bottom.x)
   local p2_min_y = math.floor(bb2.left_top.y)
+  local p2_max_x = math.ceil(bb2.right_bottom.x)
   local p2_max_y = math.ceil(bb2.right_bottom.y)
   return
     p1_min_x <= p2_max_x and
@@ -85,55 +141,99 @@ local function mark_resource_entity(force, surface, resource_entity)
 end
 
 local function mark_chunk(force, surface, chunk_position)
-  log('marking chunk ' .. serpent.block(chunk_position))
+  -- FIXME: close to working, position of tag is a bit off
+  -- position is different for same patch depending on starting chunk
 
-  -- TODO: search in adjacent chunks as long as a patch is against the edge
+  log('marking chunk ' .. serpent.block(chunk_position))
 
   -- collect all patches of the same resource
   local patches = {}
-  -- find all resources in chunk
-  local resource_entities = surface.find_entities_filtered{
-    area = chunk_area(chunk_position),
-    type = 'resource',
-  }
-  for _, resource_entity in pairs(resource_entities) do
-    -- find the index of all patches the current entity is adjacent to
-    local adjacent_patches = {}
-    for patch_idx, patch in pairs(patches) do
-      -- only consider patches with the same resource
-      if resource_entity.prototype == patch.prototype then
-        -- if the current entity is adjacent to any of the bounding boxes in
-        -- the patch, record the patch index
-        for _, patch_bb in pairs(patch.bbs) do
-          if bb_adjacent(resource_entity.bounding_box, patch_bb) then
-            table.insert(adjacent_patches, patch_idx)
+
+  -- search through chunks
+  local chunks_to_search = {chunk_position}
+  local searched_chunks = {}
+  while #chunks_to_search ~= 0 do
+    local chunk_position = table.remove(chunks_to_search)
+
+    log('searching chunk ' .. serpent.block(chunk_position))
+
+    -- this is the origin chunk if we haven't searched anything yet
+    local is_origin_chunk = #searched_chunks == 0
+
+    -- mark this chunk as searched
+    table.insert(searched_chunks, chunk_position)
+
+    -- find all resources in chunk
+    local resource_entities = surface.find_entities_filtered{
+      area = chunk_area(chunk_position),
+      type = 'resource',
+    }
+    for _, resource_entity in pairs(resource_entities) do
+      -- find the index of all patches the current entity is adjacent to
+      local adjacent_patches = {}
+      for patch_idx, patch in pairs(patches) do
+        -- only consider patches with the same resource
+        if resource_entity.prototype == patch.prototype then
+          -- if the current entity is adjacent to any of the bounding boxes in
+          -- the patch, record the patch index
+          for _, patch_bb in pairs(patch.bbs) do
+            if bb_adjacent(resource_entity.bounding_box, patch_bb) then
+              table.insert(adjacent_patches, patch_idx)
+              break
+            end
+          end
+        end
+      end
+
+      -- sort the indexes in reverse order since we will be removing them all
+      -- from the patches list
+      table.sort(adjacent_patches, function(i, j) return i > j end)
+
+      -- TODO: optimize case of only one adjacent patch
+
+      -- remove all adjacent patches and merge them into one
+      local merged_patch = {
+        prototype = resource_entity.prototype,
+        bbs = {},
+        bb = nil,
+        in_origin_chunk = is_origin_chunk,
+      }
+      for _, patch_idx in pairs(adjacent_patches) do
+        local patch = table.remove(patches, patch_idx)
+
+        merged_patch.bbs = list_concat(merged_patch.bbs, patch.bbs)
+        merged_patch.bb = merge_bbs(merged_patch.bb, patch.bb)
+        merged_patch.in_origin_chunk = merged_patch.in_origin_chunk or
+          patch.in_origin_chunk
+      end
+
+      -- add the new entity
+      table.insert(merged_patch.bbs, resource_entity.bounding_box)
+      merged_patch.bb = merge_bbs(merged_patch.bb, resource_entity.bounding_box)
+
+      -- record the merged patch
+      table.insert(patches, merged_patch)
+    end
+
+    -- remove any patches that aren't in the origin chunk
+    remove_if(patches, function(patch) return not patch.in_origin_chunk end)
+
+    -- find any neighboring chunks that have patches up against them
+    -- and make sure they get searched as well
+    for neighbor_chunk_position in cardinal_neighbors(chunk_position) do
+      if not position_list_contains(searched_chunks, neighbor_chunk_position) then
+        -- see if any patches are adjacent to the neighboring chunk
+        for _, patch in pairs(patches) do
+          if bb_adjacent(chunk_area(neighbor_chunk_position), patch.bb) then
+            table.insert(chunks_to_search, neighbor_chunk_position)
             break
           end
         end
       end
     end
-
-    -- sort the indexes in reverse order since we will be removing them all
-    -- from the patches list
-    table.sort(adjacent_patches, function(i, j) return i > j end)
-
-    -- remove all adjacent patches and merge them into one
-    local merged_patch = {
-      prototype = resource_entity.prototype,
-      bbs = {},
-    }
-    for _, patch_idx in pairs(adjacent_patches) do
-      local patch = table.remove(patches, patch_idx)
-      merged_patch.bbs = list_concat(merged_patch.bbs, patch.bbs)
-    end
-    -- add the new entity
-    table.insert(merged_patch.bbs, resource_entity.bounding_box)
-
-    -- record the merged patch
-    table.insert(patches, merged_patch)
   end
 
-  -- now mark each patch
+  -- now mark each patch found
   for _, patch in pairs(patches) do
     local tag = {}
     tag.position = bbs_center(patch.bbs)
@@ -145,9 +245,9 @@ local function mark_chunk(force, surface, chunk_position)
   end
 end
 
-script.on_event(defines.events.on_chunk_charted, function(event)
-  mark_chunk(event.force, game.surfaces[event.surface_index], event.position)
-end)
+-- script.on_event(defines.events.on_chunk_charted, function(event)
+--   mark_chunk(event.force, game.surfaces[event.surface_index], event.position)
+-- end)
 
 commands.add_command('mark-current-chunk', '', function(event)
   local player = game.players[event.player_index]
