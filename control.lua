c92c375d1f226b4b9238888faca3acb60cf10bb5
Daniel Beckwith <djbsnx@gmail.com>
2020-04-30 15:37:35 -0400

    Use ipairs, improve list removal loops

--- control.lua
+++ control.lua
@@ -22,14 +22,10 @@ local function list_reverse_pairs(t)
 end
 
 local function list_remove_if(t, f)
-  local idxs = {}
-  for idx, el in pairs(t) do
+  for idx, el in list_reverse_pairs(t) do
     if f(el) then
-      table.insert(idxs, idx)
+      table.remove(t, idx)
     end
-  end
-  for _, idx in list_reverse_pairs(idxs) do
-    table.remove(t, idx)
   end
 end
 
@@ -37,7 +33,7 @@ local function list_concat(l1, l2)
   if #l1 == 0 then
     return l2
   else
-    for _, bb in pairs(l2) do
+    for _, bb in ipairs(l2) do
       table.insert(l1, bb)
     end
     return l1
@@ -190,7 +186,7 @@ local function any_setting(players, setting)
 end
 
 local function get_resource_icon(resource_prototype)
-  for _, product in pairs(resource_prototype.mineable_properties.products) do
+  for _, product in ipairs(resource_prototype.mineable_properties.products) do
     return {
       type = product.type,
       name = product.name,
@@ -212,7 +208,9 @@ local function hide_tags(opts)
     end
   end
   for _, patch in pairs(global.patches) do
-    if (opts.force == nil or patch.force == opts.force) and patch.tag ~= nil then
+    if (opts.force == nil or patch.force == opts.force) and
+      patch.tag ~= nil
+    then
       patch.tag.destroy()
       patch.tag = nil
     end
@@ -312,7 +310,7 @@ end
 
 local function tag_chunks(chunks)
   -- look at all chunks from input that haven't been searched already
-  for _, chunk in pairs(chunks) do
+  for _, chunk in ipairs(chunks) do
     if not chunks_contains(global.searched_chunks, chunk) then
       table.insert(global.chunks_to_search, chunk)
     end
@@ -378,7 +376,9 @@ script.on_nth_tick(PROCESS_FREQUENCY, function()
   local any_new_patches = false
   local chunks_processed_this_tick = 0
   while #global.chunks_to_search ~= 0 and
-    chunks_processed_this_tick < PROCESS_BATCH do
+    chunks_processed_this_tick < PROCESS_BATCH
+  do
+    -- TODO: don't count empty chunks?
     chunks_processed_this_tick = chunks_processed_this_tick + 1
 
     local chunk = table.remove(global.chunks_to_search)
@@ -391,23 +391,9 @@ script.on_nth_tick(PROCESS_FREQUENCY, function()
       area = chunk_area(chunk),
       type = 'resource',
     }
-    for _, resource_entity in pairs(resource_entities) do
-      -- find the index of all patches the current entity is adjacent to
-      local adjacent_patches = {}
-      for patch_idx, patch in pairs(global.patches) do
-        -- only consider patches with the same resource, force, and surface
-        if resource_entity.prototype == patch.prototype and
-          chunk.force == patch.force and
-          chunk.surface == patch.surface then
-          -- if the current entity is adjacent to the patch,
-          -- record the patch index
-          if patch_adjacent(patch, resource_entity.bounding_box, true) then
-            table.insert(adjacent_patches, patch_idx)
-          end
-        end
-      end
-
-      -- remove all adjacent patches and merge them into one
+    for _, resource_entity in ipairs(resource_entities) do
+      -- find and remove all patches the current entity is adjacent to
+      -- and merge them into one
       local merged_patch = {
         prototype = resource_entity.prototype,
         bbs = {resource_entity.bounding_box},
@@ -416,23 +402,34 @@ script.on_nth_tick(PROCESS_FREQUENCY, function()
         force = chunk.force,
         surface = chunk.surface,
       }
-      for _, patch_idx in list_reverse_pairs(adjacent_patches) do
-        local patch = table.remove(global.patches, patch_idx)
+      list_remove_if(global.patches, function(patch)
+        -- merge patches with the same resource, force, and surface
+        -- that are adjacent to the current entity
+        if resource_entity.prototype == patch.prototype and
+          chunk.force == patch.force and
+          chunk.surface == patch.surface and
+          patch_adjacent(patch, resource_entity.bounding_box, true)
+        then
+          merged_patch.bbs = list_concat(merged_patch.bbs, patch.bbs)
+          merged_patch.bb = merge_bbs(merged_patch.bb, patch.bb)
+          merged_patch.amount = merged_patch.amount + patch.amount
 
-        merged_patch.bbs = list_concat(merged_patch.bbs, patch.bbs)
-        merged_patch.bb = merge_bbs(merged_patch.bb, patch.bb)
-        merged_patch.amount = merged_patch.amount + patch.amount
+          -- don't need to bother with prototype, force, and surface
+          -- since they are the same
 
-        -- don't need to bother with prototype, force, and surface,
-        -- they will be the same
+          -- clear the tag of the existing patch if it had one
+          -- one will be created for the merged patch later
+          if patch.tag ~= nil then
+            patch.tag.destroy()
+            patch.tag = nil
+          end
 
-        -- clear the tag of the existing patch if it had one
-        -- one will be created for the merged patch later
-        if patch.tag ~= nil then
-          patch.tag.destroy()
-          patch.tag = nil
+          -- remove the merged patch from the global list
+          return true
+        else
+          return false
         end
-      end
+      end)
 
       -- record the merged patch
       table.insert(global.patches, merged_patch)
@@ -453,9 +450,10 @@ script.on_nth_tick(PROCESS_FREQUENCY, function()
           neighbor_chunk.position) and
         -- TODO: make array-set structure to optimize this
         not chunks_contains(global.searched_chunks, neighbor_chunk) and
-        not chunks_contains(global.chunks_to_search, neighbor_chunk) then
+        not chunks_contains(global.chunks_to_search, neighbor_chunk)
+      then
         -- see if any patches are adjacent to the neighboring chunk
-        for _, patch in pairs(global.patches) do
+        for _, patch in ipairs(global.patches) do
           if patch_adjacent(patch, chunk_area(neighbor_chunk), false) then
             table.insert(global.chunks_to_search, neighbor_chunk)
             break
